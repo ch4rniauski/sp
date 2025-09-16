34cdac25ee1da4da2c42ab6ff2db2455b95bb6df
Yauheni Charniauski <chernyavskij.z@mail.ru>
2025-09-16 11:32:52 +0300

    Add laba5

---
+++ laba5/laba5.1/Program.cs
@@ -0,0 +1,58 @@
+﻿const int numWorkers = 5;
+const int semaphoreCapacity = 3;
+const int signalIntervalMs = 10_000;
+
+var autoResetEvent = new AutoResetEvent(false);
+var semaphore = new SemaphoreSlim(semaphoreCapacity, semaphoreCapacity);
+var obj = new object();
+
+for (var i = 0; i < numWorkers; i++)
+{
+    var id = i + 1;
+    
+    _ = Task.Run(() => Worker(id));
+}
+
+while (true)
+{
+    await Task.Delay(signalIntervalMs);
+    
+    lock (obj)
+    {
+        Console.WriteLine("\nТревога! Рабочие идут проверять участок\n");
+    }
+    
+    for (var i = 0; i < numWorkers; i++)
+    {
+        autoResetEvent.Set();
+    }
+}
+
+void Worker(int id)
+{
+    const int checkDurationMs = 2_000;
+    
+    while (true)
+    {
+        Console.WriteLine($"Рабочий {id} ждёт тревоги");
+        
+        autoResetEvent.WaitOne();
+        
+        if (semaphore.Wait(0))
+        {
+            lock (obj)
+            {
+                Console.WriteLine($"Рабочий {id} проверяет участок");
+            }
+            
+            Thread.Sleep(checkDurationMs);
+            
+            lock (obj)
+            {
+                Console.WriteLine($"Рабочий {id} закончил проверку");
+            }
+            
+            semaphore.Release();
+        }
+    }
+}
---
+++ laba5/laba5.2/Program.cs
@@ -0,0 +1,97 @@
+﻿const int numUsers = 3;
+const int coffeeBrewTimeMs = 5_000;
+const int programDurationMs = 30_000;
+
+var coffeeReadyEvent = new AutoResetEvent(false);
+var cupSemaphore = new SemaphoreSlim(1, 1);
+var consoleLock = new object();
+var stop = false;
+var tasks = new Task[numUsers];
+
+for (var i = 0; i < numUsers; i++)
+{
+    var id = i + 1;
+    
+    tasks[i] = Task.Run( () => UserThread(id) );
+}
+
+var timerEvent = new AutoResetEvent(false);
+
+var regHandle = ThreadPool.RegisterWaitForSingleObject(
+    timerEvent,
+    (_, _) =>
+    {
+        if (stop)
+        {
+            return;
+        }
+
+        lock (consoleLock)
+        {
+            Console.WriteLine("\nКофемашина: кофе готов\n");
+        }
+
+        for (var i = 0; i < numUsers; i++)
+        {
+            coffeeReadyEvent.Set();
+        }
+    },
+    null,
+    coffeeBrewTimeMs,
+    false);
+
+await using var periodicTimer = new Timer(
+    _ => timerEvent.Set(),
+    null,
+    0,
+    coffeeBrewTimeMs);
+
+await Task.Delay(programDurationMs);
+
+regHandle.Unregister(null);
+
+stop = true;
+
+for (var i = 0; i < numUsers; i++)
+{
+    coffeeReadyEvent.Set();
+}
+
+Task.WaitAll(tasks);
+
+void UserThread(int id)
+{
+    const int drinkTimeMs = 2_000;
+    
+    while (!stop)
+    {
+        lock (consoleLock)
+        {
+            Console.WriteLine($"Пользователь {id} ждёт кофе");
+        }
+
+        coffeeReadyEvent.WaitOne();
+
+        if (stop)
+        {
+            break;
+        }
+
+        if (cupSemaphore.Wait(0))
+        {
+            lock (consoleLock)
+            {
+                Console.WriteLine($"Пользователь {id} пьёт кофе");
+            }
+
+            Thread.Sleep(drinkTimeMs);
+
+            lock (consoleLock)
+            {
+                Console.WriteLine($"Пользователь {id} закончил пить кофе");
+            }
+
+            cupSemaphore.Release();
+        }
+    }
+}
